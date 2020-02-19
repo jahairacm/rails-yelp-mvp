d08609a9596ccda17cc4d00fc258a63813d72163
Jahaira Castaneda <jahairacm@gmail.com>
2020-02-19 19:18:48 +0000

    Rails Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Hello! Starting to seed the DB"
+Restaurant.create(name: "Nando's", address: "def street", category: "chinese")
+Restaurant.create(name: "Johns", address: "fwfds street", category: "belgian")
+Restaurant.create(name: "Dennys", address: "fsdf street", category: "french")
+Restaurant.create(name: "Dominos", address: "dhghfhg street", category: "italian")
+Restaurant.create(name: "Angels", address: "hghgf street", category: "japanese")
+
+puts "Finished"
